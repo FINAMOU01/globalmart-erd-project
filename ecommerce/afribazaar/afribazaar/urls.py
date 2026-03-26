d14c0022e7423e3d8247132a3822280a561fd44d
NoudouErist80 <noudoujoseph@gmail.com>
2026-03-26 02:01:54 +0100

    Add payments URL path to urlpatterns

--- ecommerce/afribazaar/afribazaar/urls.py
+++ ecommerce/afribazaar/afribazaar/urls.py
@@ -21,4 +21,5 @@ from django.views.generic import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', TemplateView.as_view(template_name='index.html'), name='home'),
+    path('payments/', include('payments.urls', namespace='payments')),
 ]
