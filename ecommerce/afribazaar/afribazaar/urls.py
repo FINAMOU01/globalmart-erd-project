1fdd043c94549815c11b93013b771d36a3df89e6
FINAMOU01 <finamouabdoul.bellegrace@ictuniversity.edu.cm>
2026-04-02 09:19:17 +0100

    Fix media files serving on Render and navbar text color to white

--- ecommerce/afribazaar/afribazaar/urls.py
+++ ecommerce/afribazaar/afribazaar/urls.py
@@ -30,6 +30,5 @@ urlpatterns = [
     path('orders/', include('orders.urls')),
 ]
 
-# Serve media files in development
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+# Serve media files in development AND production
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
